c3cfdefe7664e98396f134d1b7f44e0a5ecc2b5f
MoreToDo <donald_ricks@hotmail.com>
2016-11-13 22:19:48 -0800

    Created Basic Age Asking Program

---
+++ 01 Using Visual Studio/01 Using Visual Studio/01 Using Visual Studio.cpp
@@ -0,0 +1,25 @@
+// 01 Using Visual Studio.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream> // Input - Output library
+using namespace std; // Creates std code for all lines of C++ coding
+
+int main()
+{
+ /*  
+ cout (this is console output text)
+ cin  (this is console input text)
+ */
+
+
+   cout << "Hey this is your first GitHub tutorial" << endl; 
+   int age;
+   cout << "How old are you? ";
+   cin >> age;
+   cout << "You are " << age << " years old" << endl;
+
+   system("pause");
+    return 0;
+}
+
